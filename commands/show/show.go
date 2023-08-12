414ee8bb5a6cdaa4d2628138e69e4f350d27986b
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-12 18:15:12 +0900

    feat : update to save file from stream

--- commands/show/show.go
+++ commands/show/show.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	argu "github.com/aqyuki/rbtxt/args"
 	"github.com/aqyuki/rbtxt/stream"
 	"github.com/aqyuki/rbtxt/stream/url"
 	"github.com/spf13/cobra"
+)
+
+var (
+	SaveFilePath string
 )
 
 func ShowCommandHandler(cmd *cobra.Command, args []string) {
@@ -23,6 +28,22 @@ func ShowCommandHandler(cmd *cobra.Command, args []string) {
 		return
 	}
 	defer res.Body.Close()
+
+	if SaveFilePath != "" {
+		absPath, err := filepath.Abs(SaveFilePath)
+		if err != nil {
+			fmt.Println("Failed to output to file")
+			return
+		}
+		file, err := os.Create(absPath)
+		if err != nil {
+			fmt.Println("Failed to output to file")
+			return
+		}
+		defer file.Close()
+		stream.OutputFromStream(res.Body, file)
+		return
+	}
 
 	if stream.ExistRobots(res) {
 		stream.OutputFromStream(res.Body, os.Stdout)
