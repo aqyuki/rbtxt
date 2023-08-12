25526cf2a0af4df0b647f5b0c0143f6d14e57f55
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-12 17:15:15 +0900

    feat : add new function to show status of file exist

--- stream/output.go
+++ stream/output.go
@@ -3,6 +3,7 @@ package stream
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 // PrintFromStream outputs the contents written to the stream to standard output
@@ -12,4 +13,9 @@ func PrintFromStream(r io.Reader) {
 		return
 	}
 	fmt.Printf("%s", string(content))
+}
+
+// PrintRobotsExist outputs the status of exist file
+func PrintRobotsExist(r *http.Response) {
+	fmt.Printf("URL : %s\nExist : %t\n", r.Request.URL, ExistRobots(r))
 }
