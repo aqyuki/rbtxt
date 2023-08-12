17e8ff9086e62ff26fca4c97ad4a165945659739
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-12 14:52:36 +0900

    feat : add exist command Add exist command but not practicable

--- commands.go
+++ commands.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/aqyuki/rbtxt/commands/exist"
 	"github.com/aqyuki/rbtxt/commands/root"
 	"github.com/spf13/cobra"
 )
@@ -12,6 +13,11 @@ var (
 		Use:   "rbtxt",
 		Short: "rbtxt can quickly retrieve and check robot.txt",
 		Run:   root.RootCommandHandler,
+	}
+	ExistCmd = &cobra.Command{
+		Use:   "exist",
+		Short: "Check if robots.txt exists",
+		Run:   exist.ExistCommandHandler,
 	}
 )
 
