92d9e1f0de76de8d23c97d6652100ee944068694
aqyuki <yksk171118@gmail.com>
2023-11-27 21:41:01 +0900

    chore: change logic to get application version

--- internal/cli/command/version.go
+++ internal/cli/command/version.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 
+	"github.com/aqyuki/rbtxt/internal/info"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +17,12 @@ func CreateVersionCommand() *cobra.Command {
 		Short: "Print the version number of rbtxt",
 		Long:  `All software has versions. This is rbtxt's`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("rbtxt\nversion : %s\n", Version)
+			info, err := info.GetApplicationInformation()
+			if err != nil {
+				fmt.Printf("failed to load application information. Please report develop team.")
+				return
+			}
+			fmt.Printf("rbtxt\nversion : %s\n", info.Version)
 		},
 	}
 }
