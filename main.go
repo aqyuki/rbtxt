91f8b6a9b928f3bd30a8fe2271b96b21c224b7b1
aqyuki <yksk171118@gmail.com>
2023-11-27 21:16:02 +0900

    chore: change using function

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"os"
+
+	"github.com/aqyuki/rbtxt/internal/cli"
+)
+
 func main() {
-	CommandExecute()
+	cli := cli.CreateDefaultCLI()
+	if err := cli.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
